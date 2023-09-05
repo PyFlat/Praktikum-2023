18ccea4d711e5f0574a37132d46d2db7a2430f3c
Jonas <jonasfschlick@gmail.com>
2023-09-05 17:52:46 +0200

    added "better visualization"

--- src/main/java/org/example/gui/GraphFrame.java
+++ src/main/java/org/example/gui/GraphFrame.java
@@ -1,11 +1,13 @@
 package org.example.gui;
 
+import com.mxgraph.layout.mxCompactTreeLayout;
+import com.mxgraph.layout.hierarchical.mxHierarchicalLayout;
 import com.mxgraph.model.mxCell;
 import com.mxgraph.model.mxGeometry;
 import com.mxgraph.swing.mxGraphComponent;
 import com.mxgraph.view.mxGraph;
 import com.mxgraph.swing.handler.mxConnectionHandler;
-import org.example.data.NODE;
+import org.example.data.*;
 
 import javax.swing.*;
 import java.util.ArrayList;
@@ -15,23 +17,47 @@ public class GraphFrame extends JFrame {
         mxGraph graph = new mxGraph();
         Object parent = graph.getDefaultParent();
         graph.getModel().beginUpdate();
-
+        mxHierarchicalLayout layout = new mxHierarchicalLayout(graph);
+        layout.setOrientation(7);
         graph.setEdgeLabelsMovable(false);
         graph.setCellsEditable(false);
         graph.setAllowDanglingEdges(false);
-        graph.setCellsMovable(false);
+        //graph.setCellsMovable(false);
         graph.setCellsSelectable(false);
-
+        ArrayList<ArrayList<Object>> vertices = new ArrayList<ArrayList<Object>>();
+        ArrayList<ArrayList<NODE>> nodes = new ArrayList<ArrayList<NODE>>();
         try {
             for (int x=0;x<map.size();x++) {
+                vertices.add(new ArrayList<Object>());
+                nodes.add(new ArrayList<NODE>());
                 for (int y=0;y<map.get(x).size();y++) {
-                    graph.insertVertex(parent, null, map.get(x).get(y),x*100+(x-1)*100+100,y*50+(y-1)*30+30,100,50);
+                    vertices.get(x).add(graph.insertVertex(parent, null, map.get(x).get(y),x*100+(x-1)*100+100,y*50+(y-1)*30+30,100,50));
+                    NODE_LIKE n = NODE.database.getElementByKey(map.get(x).get(y));
+                    nodes.get(x).add((NODE) n);
                 }
-            }/*
-            Object v1 = graph.insertVertex(parent, null, "Hello", 20, 20, 80,30);
-            Object v2 = graph.insertVertex(parent, null, "World!", 24, 150,80, 30);
-            graph.insertEdge(parent, null, "Edge", v1, v2);*/
+            }
+            for (int x=0;x<map.size();x++) {
+                System.out.println(x);
+                for (int y=0;y<map.get(x).size();y++) {
+                    ArrayList<Integer> children = new ArrayList<Integer>();
+                    if (nodes.get(x).get(y).type== NODETYPE.BASIC) {continue;}
+                    else if (nodes.get(x).get(y).type == NODETYPE.SET) {children = ((SET) nodes.get(x).get(y)).getChildNodes();}
+                    else if (nodes.get(x).get(y).type == NODETYPE.SUBPATH) {children = ((SUBPATH) nodes.get(x).get(y)).getChildNodes();}
+                    else {continue;}
+                    for (int c = 0; c < children.size(); c++) {
+                        String name = NODE.database.getKeyByIndex(children.get(c));
+                        for (int x1=0;x1<map.size();x1++) {
+                            for (int y1=0;y1<map.get(x1).size();y1++) {
+                                if (map.get(x1).get(y1).equals(name)) {
+                                    graph.insertEdge(parent,null,"",vertices.get(x).get(y),vertices.get(x1).get(y1));
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         } finally {
+            layout.execute(parent);
             graph.getModel().endUpdate();
         }
         mxGraphComponent graphComponent = new mxGraphComponent(graph);
