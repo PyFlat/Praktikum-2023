899687f979c710f333358d31af1a26dbbfd1a9f7
Johannes <johannes.25406@gmail.com>
2023-09-05 16:34:07 +0200

    MxGraph settings

--- src/main/java/org/example/gui/GraphFrame.java
+++ src/main/java/org/example/gui/GraphFrame.java
@@ -1,7 +1,10 @@
 package org.example.gui;
 
+import com.mxgraph.model.mxCell;
+import com.mxgraph.model.mxGeometry;
 import com.mxgraph.swing.mxGraphComponent;
 import com.mxgraph.view.mxGraph;
+import com.mxgraph.swing.handler.mxConnectionHandler;
 
 import javax.swing.*;
 
@@ -10,11 +13,16 @@ public class GraphFrame extends JFrame {
         mxGraph graph = new mxGraph();
         Object parent = graph.getDefaultParent();
         graph.getModel().beginUpdate();
+
+        graph.setEdgeLabelsMovable(false);
+        graph.setCellsEditable(false);
+        graph.setAllowDanglingEdges(false);
+        graph.setCellsMovable(false);
+        graph.setCellsSelectable(false);
+
         try {
-            Object v1 = graph.insertVertex(parent, null, "Hello", 20, 20, 80,
-                    30);
-            Object v2 = graph.insertVertex(parent, null, "World!", 240, 150,
-                    80, 30);
+            Object v1 = graph.insertVertex(parent, null, "Hello", 20, 20, 80,30);
+            Object v2 = graph.insertVertex(parent, null, "World!", 24, 150,80, 30);
             graph.insertEdge(parent, null, "Edge", v1, v2);
         } finally {
             graph.getModel().endUpdate();
