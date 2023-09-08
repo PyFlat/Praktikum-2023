5bfba55305f5493922270bcd02424afd2a3cf78f
Jonas <jonasfschlick@gmail.com>
2023-09-08 12:22:38 +0200

    Rando Commit

--- src/main/java/org/example/gui/GraphFrame.java
+++ src/main/java/org/example/gui/GraphFrame.java
@@ -62,7 +62,7 @@ public class GraphFrame extends JFrame {
             link();
             /**/
         } finally {
-            //layout.execute(parent);
+            layout.execute(parent);
             graph.getModel().endUpdate();
         }
         mxGraphComponent graphComponent = new mxGraphComponent(graph);
