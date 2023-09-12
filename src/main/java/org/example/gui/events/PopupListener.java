669d5a9920afa3c6ce0b9f19c0b6c30886cf0ddd
Jonas <jonasfschlick@gmail.com>
2023-09-12 16:58:44 +0200

    yeeeeeeesssssss

--- src/main/java/org/example/gui/events/PopupListener.java
+++ src/main/java/org/example/gui/events/PopupListener.java
@@ -13,20 +13,21 @@ import java.util.HashMap;
 public class PopupListener implements MouseListener {
     private mxGraphComponent g;
     private HashMap<Object , Boolean> popupsOpen;
+    private HashMap<Object , JFrame> popups;
     public void discardPopup(Object cell) {
         if (popupsOpen.containsKey(cell)) {
             popupsOpen.put(cell,false);
+            popups.put(cell, null);
         }
     }
     public PopupListener(mxGraphComponent g) {
         this.g = g;
         popupsOpen = new HashMap<>();
+        popups = new HashMap<>();
     }
     public  void mouseEntered(MouseEvent e) {}
     public void mouseExited(MouseEvent e) {}
-    public void mouseClicked(MouseEvent e) {
-
-    }
+    public void mouseClicked(MouseEvent e) {}
     private void openPopup(MouseEvent event) {
         Object cell = g.getCellAt(event.getX(),event.getY());
         if (cell == null) {return;}
@@ -34,9 +35,12 @@ public class PopupListener implements MouseListener {
         if (!((mxCell) cell).isVertex()) {return;}
         if (!event.isPopupTrigger()) {return;}
         if (!popupsOpen.containsKey(cell)) {popupsOpen.put(cell, false);}
-        if (popupsOpen.get(cell)) {return;}
+        if (popupsOpen.get(cell)) {
+            popups.get(cell).toFront();
+            return;
+        }
         showPopup(event,cell);
-        popupsOpen.put(cell, true);
+
     }
     public void mousePressed(MouseEvent event) {openPopup(event);}
     public void mouseReleased(MouseEvent event) {openPopup(event);}
@@ -44,6 +48,8 @@ public class PopupListener implements MouseListener {
         JFrame popupFrame = new PropertyDisplayFrame(e.getX(), e.getY(), cell);
         popupFrame.addWindowListener(new CustomWindowAdapter(this, cell));
         popupFrame.setVisible(true);
+        popups.put(cell,popupFrame);
+        popupsOpen.put(cell, true);
     }
 }
 
